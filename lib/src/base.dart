9da68f5f64713de3675c350a89467fcd182e759a
Jairo Fernandez <jairofg12@gmail.com>
2020-12-13 01:35:07 -0500

    find request

--- lib/src/base.dart
+++ lib/src/base.dart
@@ -14,54 +14,39 @@ class Server {
 
   TeFlinkHttp server;
 
-  entryPoint() {
+  void addRoute(dynamic controller) {
+    server.addRoute(controller);
+  }
+
+  void entryPoint() {
     server.serve();
   }
 }
 
 class TeFlinkHttp {
-  TeFlinkHttp({this.port}) {
-    handler = const shelf.Pipeline()
-        .addMiddleware(shelf.logRequests())
-        .addHandler((request) => _echoRequest(request));
-  }
+  TeFlinkHttp({this.port});
 
   int port;
   dynamic handler;
+  List<dynamic> controllers = [];
+
+  void addRoute(dynamic controller) {
+    controllers.add(controller);
+  }
 
   void serve() async {
+    handler = const shelf.Pipeline()
+        .addMiddleware(shelf.logRequests())
+        .addHandler((request) => _echoRequest(request));
     var server = await io.serve(handler, _hostname, port);
     print('Serving at http://${server.address.host}:${server.port}');
   }
 
   shelf.Response _echoRequest(shelf.Request request) {
+    final controller = controllers.where((c) =>
+        c['method'].toString() == request.method &&
+        c['route'].toString() == request.url.path);
+    print(controller);
     return shelf.Response.ok('Request for "${request.url}"');
   }
-}
-
-void main(List<String> args) async {
-  var parser = ArgParser()..addOption('port', abbr: 'p');
-  var result = parser.parse(args);
-
-  // For Google Cloud Run, we respect the PORT environment variable
-  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
-  var port = int.tryParse(portStr);
-
-  if (port == null) {
-    stdout.writeln('Could not parse port value "$portStr" into a number.');
-    // 64: command line usage error
-    exitCode = 64;
-    return;
-  }
-
-  var handler = const shelf.Pipeline()
-      .addMiddleware(shelf.logRequests())
-      .addHandler((request) => _echoRequest(request));
-
-  var server = await io.serve(handler, _hostname, port);
-  print('Serving at http://${server.address.host}:${server.port}');
-}
-
-shelf.Response _echoRequest(shelf.Request request) {
-  return shelf.Response.ok('Request for "${request.url}"');
 }
